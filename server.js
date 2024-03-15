da37cca1430272539adeb461fb486d2ddc35916d
avinashid <avinashkumar532004@gmail.com>
2024-03-16 01:06:49 +0530

    "fix some"

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createRouteHandler } from "uploadthing/express";
-import uploadRouter from "./config/uploadthing.js";
+// import uploadRouter from "./config/uploadthing.js";
 import "dotenv/config";
 import fileUpload from "express-fileupload";
 import ApiRoutes from "./routes/api.js";
@@ -19,17 +19,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
 
-app.use(
-  "/api/uploadthing",
-  createRouteHandler({
-    router: uploadRouter,
-    config: {
-      uploadthingId: process.env.UPLOADTHING_APP_ID,
-      uploadthingSecret: process.env.UPLOADTHING_SECRET,
-      isDev: true,
-    },
-  })
-);
+// app.use(
+//   "/api/uploadthing",
+//   createRouteHandler({
+//     router: uploadRouter,
+//     config: {
+//       uploadthingId: process.env.UPLOADTHING_APP_ID,
+//       uploadthingSecret: process.env.UPLOADTHING_SECRET,
+//       isDev: true,
+//     },
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("Hello Server");
